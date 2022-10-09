ec3b11eab96ff094bded45fef31103e484ee09ce
ValenciaAngelinNataa <115389658+ValenciaAngelinNataa@users.noreply.github.com>
2022-10-09 00:46:04 -0700

    Add files via upload

---
+++ PRAK002-2210817220030-Valencia Angelin Nata.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+printf ("Selamat Pagi,  Valencia Angelin Nata\n");
+printf ("Selamat Siang, Valencia Angelin Nata\n");
+printf ("Selamat Malam, Valencia Angelin Nata\n");
+return 0;
+}
